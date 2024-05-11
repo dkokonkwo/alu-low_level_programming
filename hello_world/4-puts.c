6b8cc6b779ebd7f162181ab19a924c423158e1b1
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-11 13:52:44 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
+/* print line without using printf */
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return 0;}
