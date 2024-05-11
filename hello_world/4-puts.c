07da2c477d62433fef88544f3a38015a2bd0fc58
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-11 13:56:49 -0700

    parenthesis

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,5 +3,5 @@
 /* print line without using printf */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return 0;}
+puts("\"Programming is like building a multilingual puzzle");
+return (0); }
