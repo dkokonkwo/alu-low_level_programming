7fcba200c8a7283c510f15fbf4b30983cfe8940c
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-23 17:23:05 +0100

    betty1

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - prints function ten times
- * print_alphabet - prints all alphabets
+ *
  * Return: Always 0.
+ */
+
+/**
+ * print_alphabet - prints all alphabets
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
