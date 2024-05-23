34203626acd35860db254f1b09ec253fa5aae648
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-23 17:20:59 +0100

    betty and numbering

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - prints function ten times
- *
+ * print_alphabet - prints all alphabets
  * Return: Always 0.
  */
 
@@ -18,7 +18,7 @@ _putchar('\n');
 void print_alphabet_x10(void)
 {
 int a = 0;
-while (a < 11)
+while (a < 10)
 {
 print_alphabet();
 a++;
