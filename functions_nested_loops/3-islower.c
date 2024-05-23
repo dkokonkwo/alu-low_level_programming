f5cd4fe3c132089eddbabe05fe01218990515363
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-23 17:54:57 +0100

    desc

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * _islower: checks to see if integer is lowercase
+ * _islower - check if a char is lowercase
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ * On error, 0 is returned.
  */
 
 int _islower(int c)
