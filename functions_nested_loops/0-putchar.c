53821f078f24ca4afd4156a07e540e7c0141ef23
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-23 15:38:25 +0100

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+int _putchar(char c);
 /**
  * main - comment
  *
  * Return: Always 0 (Success)
  */
+char word[8] = "_putchar"; 
+int a = 0;
+
 int main()
 {
-printf("_putchar\n");
+while ( a < 8)
+{
+_putchar(word[a]);
+a++;
+}
+_putchar('\n');
 return (0);
 }
