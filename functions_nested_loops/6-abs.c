995ab7365388a273cae40f3ffb0dd5e023b25127
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-25 00:41:27 +0100

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - check if a char is lowercase
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ * On error, 0 is returned.
+ */
+int _abs(int c)
+{
+int a;
+if (c > -1)
+{
+return ("%d", c);
+}
+else
+{
+a = c * -1;
+return ("%d", a);
+}
+}
