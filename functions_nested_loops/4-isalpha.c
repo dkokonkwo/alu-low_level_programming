66dd89ecdc7975664b3baa3edaff850526366579
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-25 00:23:35 +0100

    4-isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - check if a char is lowercase
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ * On error, 0 is returned.
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
