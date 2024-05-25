025c7863b0995c50cc7efb0ae61c6158b7af1655
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-25 06:53:43 +0100

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @c: The character to check
+ *
+ * Return: On success last digit
+ */
+
+int print_last_digit(int c)
+{
+int last;
+last = c % 10;
+if (c >= 0)
+{
+_putchar (last + '0');
+return (last);
+}
+else
+{
+last = - last;
+_putchar (last + '0');
+return (last);
+}
+}
