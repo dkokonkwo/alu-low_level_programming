4f8dded8065ddcc24f3483c2f3eb606d326f7341
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-26 15:36:10 +0100

    quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
 /**
  * main - prints out string
+ *
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char message[] = "and that piece of art is useful\
+\" - Dora Korpar, 2015-10-19\n";
 write(STDERR_FILENO, message, sizeof(message) - 1);
 return (1);
 }
