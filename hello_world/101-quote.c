52fce70a97d7e75c709501a50e80a4db55a6ace4
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-26 15:20:03 +0100

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - prints out string
+ */
+
+int main(void)
+{
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, sizeof(message) - 1);
+return (1);
+}
