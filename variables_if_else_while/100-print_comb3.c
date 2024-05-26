b27776687a06144849afd4937391b58505103d1e
dkokonkwo <d.okonkwo@alustudent.com>
2024-05-26 16:55:54 +0100

    100-print_comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints two digits combination
+ *
+ * Return - 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+int b;
+a = 0;
+b = 1;
+while (a < 9)
+{
+while (b <= 9)
+{
+putchar(a + '0');
+putchar(b + '0');
+if (a < 8)
+{
+putchar(',');
+putchar(' ');
+}
+b++;
+}
+a++;
+b = a + 1;
+}
+putchar('\n');
+return (0);
+}
