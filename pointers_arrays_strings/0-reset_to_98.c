29bf6642187745a69ba0fe07bded1c906607d4aa
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-06 09:15:23 +0100

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates pointer value
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+/* main.h */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int c);
+int print_last_digit(int c);
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void times_table(void);
+void print_times_table(int n);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
