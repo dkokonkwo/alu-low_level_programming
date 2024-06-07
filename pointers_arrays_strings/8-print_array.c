84c074596cba57b2ccbcb09fbd67c85010f477ee
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-07 10:45:58 +0200

    if statement

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,32 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * _strlen - gets length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+char *ptr = s;
+while (*ptr)
+{
+++ptr;
+}
+return (ptr - s);
+}
+
+/**
  * print_array - prints n elements in array
  * @a: array
  * @n: number of elements to print
  */
 void print_array(int *a, int n)
+{
+if (n < 1)
+{
+printf("\n");
+}
+else
 {
 int i;
 for (i = 0; i < (n - 1); i++)
@@ -13,4 +34,5 @@ for (i = 0; i < (n - 1); i++)
 printf("%d, ", a[i]);
 }
 printf("%d\n", a[n - 1]);
+}
 }
