687075f43bee5616759942c1e83d19dc4aacaedc
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-07 10:37:02 +0200

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_array - prints n elements in array
+ * @a: array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+printf("%d, ", a[i]);
+}
+printf("%d\n", a[n - 1]);
+}
