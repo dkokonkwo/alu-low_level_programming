921c22de97b3354beb61d9d164b894f7f9e3e9d6
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-07 13:32:28 +0200

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - copy string to an array
+ * @dest: destination array
+ * @src: source array
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *ptr = dest;
+while (*src)
+{
+*dest++ = *src++;
+}
+*dest++ = '\0';
+return ptr;
+}
