67010a2560cc3b9cfe97a341c1f8cfea4e3d6b6a
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-19 21:13:36 -0300

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array and initializes with char
+ * @size: size of array
+ * @c: initializing character
+ *
+ * Return: NULL or pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+size_t i;
+char *ptr = (char*) malloc(sizeof(char) * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; ++i)
+{
+ptr[i] = c;
+}
+return (ptr);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,13 @@
+/* main.h */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _strlen(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
