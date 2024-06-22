6ccf51f1bcb41f3f7b60e54070e919ed87a085c0
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-22 16:56:20 -0300

    2 - 4

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - pointer to new memory space with copy of 2 concatenated strings
+ * @s1: input string 1 to copy
+ *
+ * @s2: input string 2 to copy after string 1
+ *
+ * Return: pointer to newly allocated memory with concatenated copy of strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *array;
+int i, j;
+
+i = 0;
+
+if (s1 != NULL && s2 != NULL)
+{
+i = _strlen(s1);
+i += _strlen(s2);
+}
+if (s1 == NULL && s2 == NULL)
+{
+s1 = "";
+s2 = "";
+i = 0;
+}
+else if (s1 == NULL)
+{
+s1 = "";
+i = _strlen(s2);
+}
+else if (s2 == NULL)
+{
+s2 = "";
+i = _strlen(s1);
+}
+array = (char *) malloc(sizeof(char) * (i + 1));
+if (array == NULL)
+return (0);
+i = 0;
+for (i = 0; s1[i]; i++)
+array[i] = s1[i];
+for (j = 0; s2[j]; i++, j++)
+array[i] = s2[j];
+array[i] = '\0';
+return (array);
+}
+
+/**
+ * _strlen - find string length
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+
+for (i = 0; s[i]; i++)
+continue;
+return (i);
+}
---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - pointer to two-dimensional int array
+ * @width: input number of columns in 2-D array
+ *
+ * @height: input number of rows in 2-D array
+ *
+ * Return: pointer to newly allocated memory with 2-D int array
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **array;
+int rows, columns;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+array = (int **) malloc(height * sizeof(int *));
+if (array == '\0')
+{
+return (NULL);
+}
+for (rows = 0; rows < height; rows++)
+{
+array[rows] = (int *) malloc(width * sizeof(int));
+if (array[rows] == '\0')
+{
+for (; rows >= 0; rows--)
+{
+free(array[rows]);
+}
+free(array);
+return (NULL);
+}
+}
+for (rows = 0; rows < height; rows++)
+{
+for (columns = 0; columns < width; columns++)
+{
+array[rows][columns] = 0;
+}
+}
+return (array);
+}
