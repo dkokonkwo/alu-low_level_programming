39b5f9afb74c4bb4654e8fb7b4d55b2c2d8cb936
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-28 10:48:38 -0300

    dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structures: when and why to use them. How to use typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/* dog.h */
+
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
