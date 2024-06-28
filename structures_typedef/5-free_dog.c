d2ae8ec89edb79685410bf20d297a94a91e9a89c
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-28 12:01:17 -0300

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog - frees dog_t variables in memory
@@ -7,3 +8,10 @@
 
 void free_dog(dog_t *d)
 {
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
