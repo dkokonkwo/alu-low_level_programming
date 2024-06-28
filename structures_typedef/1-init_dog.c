c105ae7a477774c24e33d4929e246ae12b5704c1
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-28 11:00:43 -0300

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#define NULL 0
+
+/* init_dog - initializes a variable dog
+ * @d: dog pointer to structure
+ * @name: name element
+ * @age: age element
+ * @owner: owner element
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
