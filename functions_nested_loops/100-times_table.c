38a665f2aedcd657514ecf2374f82da0bf6b6b30
dkokonkwo <d.okonkwo@alustudent.com>
2024-07-03 19:31:01 -0300

    times table 1

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints times table up to 15
+ * @n: table size
+ */
+
+void print_times_table(int n)
+{
+int i;
+int j;
+if (n > 15)
+{
+return;
+}
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+if (j == n)
+{
+printf("%3d\n", (i * j));
+}
+else
+{
+printf("%3d, ", (i * j));
+}
+}
+}
+}
