deb0fd476e6b615f0df34b03a1d295f72967445c
dkokonkwo <d.okonkwo@alustudent.com>
2024-07-09 18:52:52 -0300

    null check

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,9 +3,15 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: function format for printing
+ *
+ * Return: nothing if f is null
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+if (f == NULL)
+{
+return;
+}
 f(name);
 }
