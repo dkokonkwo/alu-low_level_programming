d5950f4ed430733c9a64de16a3b50a48a7b4b4c1
dkokonkwo <d.okonkwo@alustudent.com>
2024-07-19 13:24:01 -0300

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - prints letters from a text file
+ * @filename: input text file
+ * @letters: number of letters
+ *
+ * Return: 0 if file not opened/read else num of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+size_t i = 0;
+FILE *file;
+char ch;
+
+if (filename == NULL)
+{
+return (0);
+}
+
+file = fopen(filename, "r");
+if (file == NULL)
+{
+return (0);
+}
+
+while((ch = fgetc(file)) != EOF && i <= letters)
+{
+_putchar(ch);
+if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+{
+i++;
+}
+}
+fclose(file);
+return (i);
+}
---
+++ file_io/main.h
@@ -0,0 +1,7 @@
+/* main.h */
+
+#ifndef MAIN_H
+#define MAIN_H
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* main.h */
